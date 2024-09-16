a09066a9a9737f39ea6e13409a44609aeae05834
eskopp <skopp.erik@gmail.com>
2024-09-16 17:59:47 +0200

    Update exiftool.sh

--- exiftool.sh
+++ exiftool.sh
@@ -1,2 +1,7 @@
-exiftool -ImageDescription="
-Ciri, whose full name is Cirilla Fiona Elen Riannon, is one of the main characters in The Witcher 3: Wild Hunt. She is the adopted daughter of Geralt of Rivia and Yennefer of Vengerberg." content/LineArt/Ciri.png
+# Line Art
+exiftool -ImageDescription="Ciri, whose full name is Cirilla Fiona Elen Riannon, is one of the main characters in The Witcher 3: Wild Hunt. She is the adopted daughter of Geralt of Rivia and Yennefer of Vengerberg." content/LineArt/Ciri.png
+
+exiftool -ImageDescription="Line art of an anime girl." content/LineArt/Faye.png
+
+# AI Art
+exiftool -ImageDescription="as" content/AI/ai_img0.jpg
